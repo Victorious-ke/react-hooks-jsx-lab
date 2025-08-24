aa47b2f42180d1f95bc8e1b795595da94210ef53
Victorious Ngaruiya <victoriousngaruiya@gmail.com>
2025-08-24 12:54:27 +0300

    final test

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import userEvent from "@testing-library/user-event";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <Home
+      name={user.name}
+      city={user.city}
+      color={user.color}
+      />
       <About />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ import { name, city } from "../data/data.js";
 function Home() {
   
   return (<div id="home">
-    <h1 style={{color: "firebrick" }}> {name} is a web developer from {city}
-      
+    <h1 style={{ color: "firebrick" }}>
+        {name} is a Web Developer from {city}
     </h1>
     
     </div>
